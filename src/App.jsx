8145bc88300076db9206958a8683cb8e0a1d680c
Prachi8085 <prachik0311@gmail.com>
2025-04-16 16:14:34 +0530

    Added hashRouter 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import AppLayout from './layouts/app-layout';
 import LandingPage from './pages/LandingPage';
@@ -11,47 +11,59 @@ import SavedJobs from './pages/SavedJobs';
 import MyJobs from './pages/MyJobs';
 import { ThemeProvider } from './components/theme-provider';
 
-const router = createHashRouter([
-  {
-    element: <AppLayout />,
-    children: [
-      {
-        path: '/',
-        element: <LandingPage />,
-      },
-      {
-        path: '/onboarding',
-        element: <Onboarding />,
-      },
-      {
-        path: '/jobs',
-        element: <JobsListing />,
-      },
-      {
-        path: '/job/:id',
-        element: <Job />,
-      },
-      {
-        path: '/post-job',
-        element: <PostedJobs />,
-      },
-      {
-        path: '/saved-job',
-        element: <SavedJobs />,
-      },
-      {
-        path: '/my-jobs',
-        element: <MyJobs />,
-      },
-    ],
-  },
-]);
+// const router = createHashRouter([
+//   {
+//     element: <AppLayout />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <LandingPage />,
+//       },
+//       {
+//         path: '/onboarding',
+//         element: <Onboarding />,
+//       },
+//       {
+//         path: '/jobs',
+//         element: <JobsListing />,
+//       },
+//       {
+//         path: '/job/:id',
+//         element: <Job />,
+//       },
+//       {
+//         path: '/post-job',
+//         element: <PostedJobs />,
+//       },
+//       {
+//         path: '/saved-job',
+//         element: <SavedJobs />,
+//       },
+//       {
+//         path: '/my-jobs',
+//         element: <MyJobs />,
+//       },
+//     ],
+//   },
+// ]);
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="onboarding" element={<Onboarding />} />
+          <Route path="jobs" element={<JobsListing />} />
+          <Route path="job/:id" element={<Job />} />
+          <Route path="post-job" element={<PostedJobs />} />
+          <Route path="saved-job" element={<SavedJobs />} />
+          <Route path="my-jobs" element={<MyJobs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
   );
 }
 
