b7e3adc325a56a8037e5ca802f7412c8f4488382
Prachi8085 <prachik0311@gmail.com>
2025-04-16 14:12:07 +0530

    Added hashRouter 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import { Button } from './components/ui/button';
 import AppLayout from './layouts/app-layout';
 import LandingPage from './pages/LandingPage';
 import Onboarding from './pages/Onboarding';
@@ -12,25 +11,48 @@ import SavedJobs from './pages/SavedJobs';
 import MyJobs from './pages/MyJobs';
 import { ThemeProvider } from './components/theme-provider';
 
+const router = createHashRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <LandingPage />,
+      },
+      {
+        path: '/onboarding',
+        element: <Onboarding />,
+      },
+      {
+        path: '/jobs',
+        element: <JobsListing />,
+      },
+      {
+        path: '/job/:id',
+        element: <Job />,
+      },
+      {
+        path: '/post-job',
+        element: <PostedJobs />,
+      },
+      {
+        path: '/saved-job',
+        element: <SavedJobs />,
+      },
+      {
+        path: '/my-jobs',
+        element: <MyJobs />,
+      },
+    ],
+  },
+]);
+
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Router>
-        <AppLayout>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/onboarding" element={<Onboarding />} />
-            <Route path="/jobs" element={<JobsListing />} />
-            <Route path="/job/:id" element={<Job />} />
-            <Route path="/post-job" element={<PostedJobs />} />
-            <Route path="/saved-job" element={<SavedJobs />} />
-            <Route path="/my-jobs" element={<MyJobs />} />
-          </Routes>
-        </AppLayout>
-      </Router>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
 
 export default App;
-
